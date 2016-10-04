05b65ff76a85a200f4ac9a238b26982249cbf463
bgveenstra <b.g.veenstra@gmail.com>
2016-10-04 01:35:04 -0700

    checked solution

--- lib_app/db/schema.rb
+++ lib_app/db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -29,10 +28,9 @@ ActiveRecord::Schema.define(version: 20160113214541) do
     t.integer  "library_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["library_id"], name: "index_library_users_on_library_id", using: :btree
+    t.index ["user_id"], name: "index_library_users_on_user_id", using: :btree
   end
-
-  add_index "library_users", ["library_id"], name: "index_library_users_on_library_id", using: :btree
-  add_index "library_users", ["user_id"], name: "index_library_users_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
